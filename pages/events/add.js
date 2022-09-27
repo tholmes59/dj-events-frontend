9e075e4a1a4cbbbc772af6e20fa86c55758f2974
Thomas <tholmes59@gmail.com>
2022-09-26 18:08:20 -0700

    create add event page

---
+++ pages/events/add.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../../components/Layout";
+
+function AddEventPage() {
+  return (
+    <Layout title="Add New Event">
+      <h1>Add Event</h1>
+    </Layout>
+  );
+}
+
+export default AddEventPage;
