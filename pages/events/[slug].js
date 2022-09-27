2bd012496a44681e3e95b3ed98f396ecb5ae754e
Thomas <tholmes59@gmail.com>
2022-09-27 15:43:53 -0700

    create page for individual events with event id slug and import app layout

---
+++ pages/events/[slug].js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../../components/Layout";
+
+function EventPage() {
+  return (
+    <Layout>
+      <h1>My Event 1</h1>
+    </Layout>
+  );
+}
+
+export default EventPage;
