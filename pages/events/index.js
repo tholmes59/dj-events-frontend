c069d60bbe7462b6abcafacb80b1084752887e86
Thomas <tholmes59@gmail.com>
2022-09-26 17:54:13 -0700

    create events page

---
+++ pages/events/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../../components/Layout";
+
+function EventsPage() {
+  return (
+    <Layout>
+      <h1>My Events</h1>
+    </Layout>
+  );
+}
+
+export default EventsPage;
