e23aa2718339dadfcea12fcc65dcba1544daf913
Thomas <tholmes59@gmail.com>
2022-09-29 10:50:01 -0700

    create getStaticProps function to fetch DJ events and display them and to create a button to view all events on the events page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,34 @@
 import Layout from "@/components/Layout";
+import EventItem from "@/components/EventItem";
+import { API_URL } from "@/config/index";
+import Link from "next/link";
+import Events from "./events";
 
-export default function HomePage() {
+export default function HomePage({ evnt }) {
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1>Upcoming Events</h1>
+      {evnt.length === 0 ?? <h3>No events to show</h3>}
+      {evnt.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
+
+      {evnt.length > 0 && (
+        <Link href="/events">
+          <a className="btn-secondary">View All Events</a>
+        </Link>
+      )}
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const evnt = await fetch(`${API_URL}/api/events`)
+    .then((res) => res.json())
+    .then((data) => data);
+
+  return {
+    props: { evnt },
+    revalidate: 1,
+  };
 }
