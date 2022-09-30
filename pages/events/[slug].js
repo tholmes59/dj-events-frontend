eda28c285b367182a9c098034748e553f050eddf
Thomas <tholmes59@gmail.com>
2022-09-30 08:08:26 -0700

    build out page displaying individual events

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,12 +1,65 @@
 import React from "react";
 import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
+import styles from "@/styles/Event.module.css";
+import Link from "next/link";
+import Image from "next/image";
+import { FaPencilAlt, FaTimes } from "react-icons/fa";
 
-function EventPage() {
+export default function EventPage({ evt }) {
+  console.log(evt);
+
+  const deleteEvent = (e) => {
+    console.log("delete");
+  };
   return (
     <Layout>
-      <h1>My Event 1</h1>
+      <div className={styles.event}>
+        <div className={styles.controls}>
+          <Link href={`/events/edit/${evt.id}`}>
+            <a>
+              <FaPencilAlt /> Edit Event
+            </a>
+          </Link>
+          <a href="#" className={styles.delete} onClick={deleteEvent}>
+            <FaTimes /> Delete Event
+          </a>
+        </div>
+
+        <span>
+          {evt.date} at {evt.time}
+        </span>
+        <h1>{evt.name}</h1>
+        {evt.image && (
+          <div className={styles.image}>
+            <Image src={evt.image} width={960} height={600} alt="Event Image" />
+          </div>
+        )}
+        <h3>Performers:</h3>
+        <p>{evt.performers}</p>
+        <h3>Description:</h3>
+        <p>{evt.description}</p>
+        <h3>Venue: {evt.venue}</h3>
+        <p>{evt.address}</p>
+
+        <Link href="/events">
+          <a className={styles.back}>{"<"} Go Back</a>
+        </Link>
+      </div>
     </Layout>
   );
 }
 
-export default EventPage;
+export async function getServerSideProps({ query: { slug } }) {
+  console.log(slug);
+
+  const singleEvnt = await fetch(`${API_URL}/api/events/${slug}`)
+    .then((res) => res.json())
+    .then((data) => data);
+
+  return {
+    props: {
+      evt: singleEvnt[0],
+    },
+  };
+}
